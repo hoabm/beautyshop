519574de68c39ead6088060badab1eab6d514e4e
hoabm <hoaxg276@gmail.com>
2019-06-21 11:06:50 +0700

    Xay dung domain Entities 2

---
+++ BeautyShop.Model/Models/Footer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyShop.Model.Models
+{
+    [Table("Footers")] //Maping toi bang Footers
+    public class Footer
+    {
+        [Key] //khóa chính
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required] //bắt buộc phải nhập
+        public string Content { set; get; }
+    }
+}
---
+++ BeautyShop.Model/Models/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string URL { set; get; }
+
+        public int? DisplayOrder { set; get; }
+
+        [Required]
+        public int GroupID { set; get; }
+
+        [ForeignKey("GroupID")]  //trỏ lên GroupID ở trên
+        public virtual MenuGroup MenuGroup { set; get; } //Khóa ngoại GroupID chiếu tới bảng MenuGroup
+
+        [MaxLength(50)]
+        public string Target { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
